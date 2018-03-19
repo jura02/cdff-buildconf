ea0e6fc5173d4d73146a49a47476f303ac31b6d9
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-03-19 19:23:42 +0100

    removed dfkigit

--- init.rb
+++ init.rb
@@ -25,7 +25,6 @@
 
 require 'autoproj/git_server_configuration'
 Autoproj.git_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
-Autoproj.git_server_configuration('DFKIGIT', 'git.hb.dfki.de', :fallback_to_http => false, default: 'ssh,ssh', disabled_methods: 'http,git')
 Autoproj.git_server_configuration('SPACEAPP', 'gitlab.spaceapplications.com', :fallback_to_http => false, default: 'ssh,ssh', disabled_methods: 'http,git')
 
 Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
